45bfa7ef9a9593f30691759b42ace67a2586250d
Adyantto <adiyantond@gmail.com>
2026-07-21 13:44:06 +0800

    Update guru-piket.js

--- data/guru-piket.js
+++ data/guru-piket.js
@@ -1,1 +1,160 @@
+/* =====================================================
+   DATABASE GURU PIKET
+   SMA NEGERI 2 SENDAWAR
+   Semester Ganjil Tahun Pelajaran 2026 / 2027
+   ===================================================== */
+
+const dataGuruPiket = [
+
+  /* ================= SENIN ================= */
+{
+  hari: "Senin",
+  jadwal: [
+    {
+      waktu: "07.30 - 10.00",
+      guru: [
+        "Mahdalena, S.Pd., Gr.",
+        "Sumarmi, S.Pd., Gr.",
+        "Al Misbah Hajihi, S.Pd"
+      ]
+    },
+    {
+      waktu: "10.00 - 13.00",
+      guru: [
+        "Agustinus Bhakti Parrangan, S.Pd., M.Pd",
+        "Maleaki Karaeng Lolo, S.Pd.K.",
+        "Sopiana, S.Th., Gr."
+      ]
+    },
+    {
+      waktu: "13.00 - 16.00",
+      guru: [
+        "Marlin Mario Eva Sumual, S.Pd.K., MA., Gr.",
+        "M. Nasir, S.Sy., Gr.",
+        "Susilawati, S.Pd"
+      ]
+    }
+  ]
+},
+
+  /* ================= SELASA ================= */
+{
+  hari: "Selasa",
+  jadwal: [
+    {
+      waktu: "07.30 - 10.00",
+      guru: [
+        "Jasmir, S.Pd., Gr.",
+        "Maria Dwi O.L., S.Pd., Gr.",
+        "Agustinus Bhakti Parrangan, S.Pd., M.Pd"
+      ]
+    },
+    {
+      waktu: "10.00 - 13.00",
+      guru: [
+        "Sopiana, S.Th., Gr.",
+        "Katrina Pareallo, S.Si., Gr.",
+        "Al Misbah Hajihi, S.Pd"
+      ]
+    },
+    {
+      waktu: "13.00 - 16.00",
+      guru: [
+        "Vidrich Dendi, S.Pd., Gr.",
+        "Fidelis Anastasius F., S.Pd., Gr.",
+        "Febe Novita Sari, S.Pd.K"
+      ]
+    }
+  ]
+},
+
+
+/* ================= RABU ================= */
+{
+  hari: "Rabu",
+  jadwal: [
+    {
+      waktu: "07.30 - 10.00",
+      guru: [
+        "Mahdalena, S.Pd., Gr.",
+        "Mira Henny Asmiati, S.Pd., Gr.",
+        "Maleaki Karaeng Lolo, S.Pd.K."
+      ]
+    },
+    {
+      waktu: "10.00 - 13.00",
+      guru: [
+        "Febe Novita Sari, S.Pd.K",
+        "Nurmaya Sihotang, S.Pd., M.Pd., Gr.",
+        "Rifky Ibnu Abdul A., S.Pd., Gr."
+      ]
+    },
+    {
+      waktu: "13.00 - 16.00",
+      guru: [
+        "Susilawati, S.Pd",
+        "Muhammad Nasir, S.Pd., Gr.",
+        "Romi Puspita, S.Pd., Gr."
+      ]
+    }
+  ]
+},
+
+
+/* ================= KAMIS ================= */
+{
+  hari: "Kamis",
+  jadwal: [
+    {
+      waktu: "07.30 - 10.00",
+      guru: [
+        "M. Nasir, S.Sy., Gr.",
+        "Sopiana, S.Th., Gr.",
+        "Meydi Susanti, S.Pd"
+      ]
+    },
+    {
+      waktu: "10.00 - 13.00",
+      guru: [
+        "Maria Dwi O.L., S.Pd., Gr.",
+        "Nurmaya Sihotang, S.Pd., M.Pd., Gr.",
+        "Marlin Mario Eva Sumual, S.Pd.K., MA., Gr."
+      ]
+    },
+    {
+      waktu: "13.00 - 16.00",
+      guru: [
+        "Muhammad Nasir, S.Pd., Gr.",
+        "Emiliana, S.Pd",
+        "Yuvenalis Raga Gening, S.Ag., M.Th"
+      ]
+    }
+  ]
+},
+
+
+ /* ================= JUMAT ================= */
+{
+  hari: "Jumat",
+  jadwal: [
+    {
+      waktu: "07.30 - 09.00",
+      guru: [
+        "Sumarmi, S.Pd., Gr.",
+        "Yuvenalis Raga Gening, S.Ag., M.Th",
+        "Katrina Pareallo, S.Si., Gr."
+      ]
+    },
+    {
+      waktu: "09.00 - 11.00",
+      guru: [
+        "Maleaki Karaeng Lolo, S.Pd.K.",
+        "Vidrich Dendi, S.Pd., Gr.",
+        "Jasmir, S.Pd., Gr."
+      ]
+    }
+  ]
+}
+
+];
 
